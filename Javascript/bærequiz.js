6c85bca56fef91f9c661b734b36006c7daeca983
javedkubbe <ejmuj1@gmail.com>
2025-01-09 11:22:18 +0100

    bærekraftsquiz virker

--- Javascript/bærequiz.js
+++ Javascript/bærequiz.js
@@ -1,120 +1,68 @@
-let poeng = 0
-let totalPoeng = 0
+let poeng1 = 0
+let poeng2 = 0
+let poeng3 = 0
+let poeng4 = 0
 
-document.getElementById("svar").addEventListener("click", chooseAnswer1, chooseAnswer2, chooseAnswer3, chooseAnswer4);
+let totalPoeng = 4
+
+function updateResult() {
+    const totalScore = poeng1 + poeng2 + poeng3 + poeng4;
+    document.getElementById("resultat").innerText = 'Du fikk ' + totalScore + ' av ' + totalPoeng + ' Poeng';
+}
 
 function chooseAnswer1(){
     const operator = document.getElementById("yAnswer").value;
-
-    let result;
-
-    switch (operator){
-        case 'a':
-            totalPoeng++;
-            break;
-
-        case 'b':
-            totalPoeng++;
-            poeng++;
-            break;
-
-        case 'c':
-            totalPoeng++;
-            break;
-    
-        case 'd':
-            totalPoeng++;
-            break;
-
-        default:
-            result = "Velg svar"
+    if(operator === "a"){
+        poeng1 = 1;
     }
+    else {
+        poeng1 = 0
+    }
+    updateResult();
+    document.getElementById("poen").innerText = "Poeng:" + poeng1
 }
 
 function chooseAnswer2(){
     const operator = document.getElementById("yAnswer2").value;
 
-    let result;
-
-    switch (operator){
-        case 'a':
-            totalPoeng++;
-            break;
-
-        case 'b':
-            totalPoeng++;
-            break;
-
-        case 'c':
-            totalPoeng++;
-            poeng++;
-            break;
-    
-        case 'd':
-            totalPoeng++;
-            break;
-
-        default:
-            result = "Velg svar"
+    if(operator === "d"){
+        poeng2 = 1;
     }
+    else {
+        poeng2 = 0
+    }
+    updateResult();
+    document.getElementById("poen2").innerText = "Poeng:" + poeng2
 }
 
 function chooseAnswer3(){
     const operator = document.getElementById("yAnswer3").value;
 
-    let result;
-
-    switch (operator){
-        case 'a':
-            totalPoeng++;
-            poeng++;
-            break;
-
-        case 'b':
-            totalPoeng++;
-            poeng++;
-            break;
-
-        case 'c':
-            totalPoeng++;
-            break;
-    
-        case 'd':
-            totalPoeng++;
-            break;
-
-        default:
-            result = "Velg svar"
+    if(operator === "b"){
+        poeng3 = 1;
     }
+    else {
+        poeng3 = 0
+    }
+    updateResult();
+    document.getElementById("poen3").innerText = "Poeng:" + poeng3
 }
 
 function chooseAnswer4(){
     const operator = document.getElementById("yAnswer4").value;
 
-    let result;
-
-    switch (operator){
-        case 'a':
-            totalPoeng++;
-            break;
-
-        case 'b':
-            totalPoeng++;
-            poeng++;
-            break;
-
-        case 'c':
-            totalPoeng++;
-            break;
-    
-        case 'd':
-            totalPoeng++;
-            poeng++;
-            break;
-
-        default:
-            result = "Velg svar"
+    if(operator === "c"){
+        poeng4 = 1;
     }
+    else {
+        poeng4 = 0
+    }
+    updateResult();
+    document.getElementById("poen4").innerText = "Poeng:" + poeng4
 }
 
-document.getElementById("resultat").innerText = 'Du fikk' + poeng + "av" + totalPoeng + "poeng"
+
+document.getElementById("svar1").addEventListener("click", chooseAnswer1);
+document.getElementById("svar2").addEventListener("click", chooseAnswer2);
+document.getElementById("svar3").addEventListener("click", chooseAnswer3);
+document.getElementById("svar4").addEventListener("click", chooseAnswer4);
